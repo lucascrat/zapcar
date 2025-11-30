4e13cf75b5d7544837f9795fcad96bbecebd2570
ZapCar Team <lucascrat@github.com>
2025-11-30 03:28:34 -0300

    feat: Implement native Android PiP support in MainActivity

--- android/app/src/main/java/br/com/client/chegoja/MainActivity.java
+++ android/app/src/main/java/br/com/client/chegoja/MainActivity.java
@@ -1,5 +1,53 @@
 package br.com.client.chegoja;
 
+import android.os.Bundle;
+import android.os.Build;
+import android.util.Rational;
+import android.app.PictureInPictureParams;
+import android.webkit.JavascriptInterface;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Injeta a interface JS "Android" no WebView do Capacitor
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().addJavascriptInterface(new WebAppInterface(this), "Android");
+        }
+    }
+
+    public class WebAppInterface {
+        MainActivity mActivity;
+
+        WebAppInterface(MainActivity c) {
+            mActivity = c;
+        }
+
+        @JavascriptInterface
+        public void enterPipMode() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                try {
+                    // Define a proporção da janela PiP (Vertical 3:4)
+                    Rational aspectRatio = new Rational(3, 4);
+                    PictureInPictureParams params = new PictureInPictureParams.Builder()
+                            .setAspectRatio(aspectRatio)
+                            .build();
+                    mActivity.enterPictureInPictureMode(params);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @JavascriptInterface
+        public void bringToFront() {
+            // Traz a atividade para o topo
+            android.content.Intent intent = new android.content.Intent(mActivity, MainActivity.class);
+            intent.setFlags(android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+                    | android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+            mActivity.startActivity(intent);
+        }
+    }
+}
